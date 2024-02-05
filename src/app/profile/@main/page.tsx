e7ba7133953ff1fe737d67180026e193fc4c9183
faizanalibaig <baig4992@gmail.com>
2024-02-05 20:44:58 +0500

    changeszz

--- src/app/profile/@main/page.tsx
+++ src/app/profile/@main/page.tsx
@@ -2,16 +2,39 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 
+import { FaHtml5 } from "react-icons/fa";
+import { SiCsswizardry } from "react-icons/si";
+import { IoLogoJavascript } from "react-icons/io5";
+import { SiPreact } from "react-icons/si";
+import { SiNextdotjs } from "react-icons/si";
+import { SiTypescript } from "react-icons/si";
+import { IoLogoFigma } from "react-icons/io5";
+import { SiCanva } from "react-icons/si";
+import { SiMongodb } from "react-icons/si";
+import { FaJava } from "react-icons/fa6";
+
+
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
+import { SkillsData } from '@/dbData/SkillsData'
 import {ProjectData} from "@/dbData/ProjectsData"
-
-
 import ImageMan from '@/assets/360_F_214746128_31JkeaP6rU0NzzzdFC4khGkmqc8noe6h.jpg'
+import CImage from "@/assets/letter-c.png"
 
 function Main() {
   return (
     <main className='mt-[124px] w-screen h-auto flex flex-col gap-28'>
       <MainInfo/>
       <Caursols/>
+      <TechnicalExpertise/>
     </main> 
   
   )
@@ -69,7 +92,7 @@ function Caursols(){
          
           {
             ProjectData.map((value, index)=>(
-              <div key={index} className='h-[320px] min-w-[210px] sm:h-[320px] sm:min-w-[250px] md:h-[340px] md:min-w-[260px] lg:h-[360px] lg:min-w-[250px] xl:h-[420px] xl:min-w-[320px] bg-blue-600 relative'>
+              <div key={index} className='h-[340px] min-w-[210px] sm:h-[340px] sm:min-w-[250px] md:h-[340px] md:min-w-[260px] lg:h-[360px] lg:min-w-[250px] xl:h-[420px] xl:min-w-[320px] bg-blue-600 relative'>
                 <h1 className='absolute top-24 right-[-75px] sm:top-28 sm:right-[-75px] lg:top-32 lg:right-[-78px] xl:top-36 xl:right-[-80px] text-2xl sm:text-2xl lg:text-3xl xl:text-3xl w-44 h-auto font-bold'>{value.Title}</h1>
                 <h1 className='absolute right-[-25px] top-5 sm:right-[-25px] sm:top-5 lg:right-[-30px] lg:top-5 font-medium text-lg sm:text-lg lg:text-xl xl:text-2xl'>{value.Number}</h1>
                 <Link  href="" className='absolute right-[-12px] top-56 sm:right-[-14px] sm:top-64 lg:right-[-16px] lg:top-72 xl:right-[-20px] xl:top-80 text-lg sm:text-xl border-b-2 border-black'>{value.Github}</Link>
@@ -78,6 +101,120 @@ function Caursols(){
           }
           
        </div>
+    </div>
+  )
+}
+
+
+function TechnicalExpertise(){
+  return(
+    <div className='h-auto w-full  py-20 flex flex-col gap-8 lg:flex-row'>
+          <div className='h-full lg:w-1/2 px-5 sm:px-16 lg:px-18 xl:px-20 '>
+                  <Table>
+                   <TableHeader className='bg-black/[2%]'>
+                   <TableRow>
+                          <TableHead className="w-full h-16 font-semibold text-black text-base sm:text-xl md:text-xl lg:text-lg xl:text-xl">Front-end Development Expertise</TableHead>
+                          <TableHead className="w-full h-16 font-semibold text-black text-xl"></TableHead>
+
+                 </TableRow>
+                        
+                 </TableHeader>
+                  <TableBody>
+                         <TableRow >
+                       <TableCell className='text-black text-medium text-sm sm:text-lg md:text-lg lg:text-base xl:text-lg font-medium h-12'>Hypertext Markup Language (HTML)</TableCell>
+         
+                       <TableCell className='text-black text-medium text-xl sm:text-2xl'><FaHtml5 /></TableCell>
+                       </TableRow>    
+
+                       <TableRow >
+
+                       <TableCell className='text-black text-medium text-sm sm:text-lg md:text-lg lg:text-base xl:text-lg  font-medium h-12'>Cascading Style Sheets (CSS)</TableCell>
+
+                       <TableCell className='text-black text-medium text-xl sm:text-2xl'><SiCsswizardry /></TableCell>
+                       </TableRow>    
+
+                       <TableRow >
+
+                       <TableCell className='text-black text-medium text-sm sm:text-lg md:text-lg lg:text-base xl:text-lg  font-medium h-12'>Javascript</TableCell>
+
+                       <TableCell className='text-black text-medium text-xl sm:text-2xl'><IoLogoJavascript/></TableCell>
+                       </TableRow>    
+
+                       <TableRow >
+
+                        <TableCell className='text-black text-medium text-sm sm:text-lg md:text-lg lg:text-base xl:text-lg  font-medium h-12'>ReactJS</TableCell> 
+
+                        <TableCell className='text-black text-medium text-xl sm:text-2xl'><SiPreact/></TableCell>
+                        </TableRow>    
+
+                        <TableRow >
+
+                        <TableCell className='text-black text-medium text-sm sm:text-lg md:text-lg lg:text-base xl:text-lg  font-medium h-12'>NextJs</TableCell> 
+
+                        <TableCell className='text-black text-medium text-xl sm:text-2xl'><SiNextdotjs/></TableCell>
+                        </TableRow>    
+
+                        <TableRow >
+
+                        <TableCell className='text-black text-medium text-sm sm:text-lg md:text-lg lg:text-base xl:text-lg  font-medium h-12'>Typescript</TableCell>
+
+                        <TableCell className='text-black text-medium text-xl sm:text-2xl'><SiTypescript/></TableCell>
+
+                          </TableRow> 
+                          <TableRow >
+                         <TableCell className='text-black text-medium text-sm sm:text-lg md:text-lg lg:text-base xl:text-lg  font-medium h-12'>MongoDB</TableCell>
+                         <TableCell className='text-black text-medium text-xl sm:text-2xl'><SiMongodb /></TableCell>
+                       </TableRow>    
+             </TableBody>
+             </Table>
+          </div>
+          <div className='lg:w-1/2 px-4 sm:px-16 lg:px-18 xl:px-20 flex flex-col gap-8'>
+               <div className='h-auto w-full'>
+               <Table>
+               <TableHeader className='bg-black/[2%]'>
+                   <TableRow>
+                          <TableHead className="w-full h-14 font-semibold text-black text-base  sm:text-xl md:text-xl lg:text-lg xl:text-xl">Development Software Expertise</TableHead>
+                          <TableHead className="w-full h-16 font-semibold text-black text-xl"></TableHead>
+                 </TableRow>
+                 </TableHeader>
+                   <TableBody>
+                        <TableRow >
+                         <TableCell className='text-black text-medium text-sm sm:text-lg md:text-lg lg:text-base xl:text-lg  font-medium h-12'>Figma</TableCell>
+                         <TableCell className='text-black text-medium text-xl sm:text-2xl'><IoLogoFigma /></TableCell>
+                       </TableRow> 
+                       <TableRow >
+                         <TableCell className='text-black text-medium text-sm sm:text-lg md:text-lg lg:text-base xl:text-lg  font-medium h-12'>Canva</TableCell>
+                         <TableCell className='text-black text-medium text-xl sm:text-2xl'><SiCanva /></TableCell>
+                       </TableRow> 
+                         
+                 </TableBody>
+                   </Table>
+               </div>
+               <div className='h-auto w-full'>
+               <Table>
+               <TableHeader className='bg-black/[2%]'>
+                   <TableRow>
+                          <TableHead className="w-full h-16 font-semibold text-black text-base  sm:text-xl md:text-xl lg:text-lg xl:text-xl">Courses</TableHead>
+                          <TableHead className="w-full h-16 font-semibold text-black text-xl"></TableHead>
+                 </TableRow>
+                 </TableHeader>
+                   <TableBody>
+                        <TableRow >
+                         <TableCell className='text-black text-medium text-sm sm:text-lg md:text-lg lg:text-base xl:text-lg  font-medium h-12'>Programming Fundamentals</TableCell>
+                         <TableCell className='text-black text-medium '><Image src={CImage} alt='' className='w-5 sm:w-6 h-auto'/></TableCell>
+                       </TableRow> 
+                       <TableRow >
+                         <TableCell className='text-black text-medium text-sm sm:text-lg md:text-lg lg:text-base xl:text-lg  font-medium h-12'>Object Oriented Programming</TableCell>
+                         <TableCell className='text-black text-medium text-xl sm:text-2xl'><FaJava /></TableCell>
+                       </TableRow>   
+                       <TableRow >
+                         <TableCell className='text-black text-medium text-sm sm:text-lg md:text-lg lg:text-base xl:text-lg  font-medium h-12'>DSA</TableCell>
+                         <TableCell className='text-black text-medium text-2xl'><Image src={CImage} alt='' className='w-5 h-auto'/></TableCell>
+                       </TableRow> 
+                 </TableBody>
+                   </Table>
+               </div>
+          </div>
     </div>
   )
 }
---
+++ src/dbData/SkillsData.ts
@@ -0,0 +1,33 @@
+
+export const SkillsData=[
+    {
+        key:1,
+        title:"Hypertext Markup Language (HTML)",
+        img:"",
+    },
+    {
+        key:2,
+        title:"Cascading Style Sheets (CSS)",
+        img:"",
+    },
+    {
+        key:3,
+        title:"Javascript",
+        img:"",
+    },
+    {
+        key:4,
+        title:"ReactJS",
+        img:"",
+    },
+    {
+        key:5,
+        title:"NextJs",
+        img:"",
+    },
+    {
+        key:6,
+        title:"Typescript",
+        img:"",
+    }
+]

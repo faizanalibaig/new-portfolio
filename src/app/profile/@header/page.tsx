b523926e5764a9054bf020aaf8b01f551ce31858
faizanalibaig <baig4992@gmail.com>
2024-02-05 13:26:39 +0500

    new change

--- src/app/profile/@header/page.tsx
+++ src/app/profile/@header/page.tsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <header className={`py-8 px-8 sm:py-10 sm:px-12 fixed top-0 left-0 right-0 w-screen h-auto bg-transparent`}>
         <section className='flex justify-between items-center'>
-            <h1 className='text-[28px] sm:text-3xl font-semibold cursor-default'>
+            <h1 className='text-[26px] sm:text-3xl font-semibold cursor-default'>
                 faizan
                 <span className='text-black/60'>
                 ali.
@@ -13,7 +13,7 @@ function Header() {
             </h1>
             <VscThreeBars className='block sm:hidden text-[28px] cursor-pointer'/>
             
-           <button className='hidden sm:block bg-black h-[44px] w-[104px] text-white text-[18px] rounded-[6px] 
+           <button className='hidden sm:block bg-black sm:h-[38px] sm:w-[92px] sm:text-[16px] xl:h-[44px] xl:w-[104px] text-white xl:text-[18px] rounded-[6px] 
            hover:rounded-[20px] transition-all font-medium'>
             resume</button> 
         </section>
--- src/app/profile/@main/page.tsx
+++ src/app/profile/@main/page.tsx
@@ -8,11 +8,11 @@ function Main() {
     <main className='mt-[124px] w-screen h-auto pt-10 lg:py-6 xl:py-10'>
       <section className='w-screen flex justify-around md:h-[550px] lg:flex-row lg:h-[450px] xl:h-[500px]'>
         {/* Image */}
-        <div className='hidden lg:block lg:w-[450px] xl:w-[600px] h-full '>
+        <div className='hidden lg:block lg:w-[380px] xl:w-[600px] h-full '>
           <Image src={ImageMan} alt='profile pic' className='h-full w-full'/>
         </div>
         {/* Information */}
-        <section className='w-[80%] lg:w-[420px] xl:w-[560px] h-full  flex gap-2 md:gap-0 flex-col'>
+        <section className='w-[80%] lg:w-[380px] xl:w-[560px] h-full  flex gap-2 md:gap-0 flex-col'>
            <div className='w-full bg-white h-[20%] sm:h-auto '>
             <h1 className='flex flex-col font-semibold text-5xl sm:text-8xl sm:py-10 md:text-8xl md:py-10 lg:text-7xl lg:py-0 xl:text-8xl'>
               <span>
